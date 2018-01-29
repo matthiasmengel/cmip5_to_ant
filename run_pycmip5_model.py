2025d777409469f8dfda83671bf5005b34297d45
Matthias Mengel <mengel@pik-potsdam.de>
2018-01-29 14:52:28 +0100

    first working version for one model.

---
+++ run_pycmip5_model.py
@@ -0,0 +1,61 @@
+
+import os
+import glob
+import jinja2
+import numpy as np
+import netCDF4 as nc
+import settings
+
+source_path = os.path.join(settings.pcmdipath,settings.scenarios[0],
+    settings.time_res,settings.variable,settings.models[0],
+    settings.cmip5_runid)
+
+runid = "_".join([settings.variable,settings.time_res,
+    settings.models[0],settings.scenarios[0],settings.cmip5_runid])
+
+target_path = os.path.join(settings.target_path, runid)
+
+cmip_files = sorted(glob.glob(os.path.join(source_path,"*")))
+cmip_files = [os.path.basename(f) for f in cmip_files]
+
+# make jinja aware of templates in the pism_input/tools folder
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(
+            searchpath=os.path.join(settings.project_root,"templates")))
+
+
+# get z axis
+ncf = nc.Dataset(os.path.join(source_path,cmip_files[0]), 'r')
+nc_zlevels = ncf.variables['lev'][:]
+ncf.close()
+
+def find_nearest(array,range_to_average):
+
+  idx0=(np.abs(array-range_to_average[0])).argmin()
+  idx1=(np.abs(array-range_to_average[1])).argmin()
+  return ",".join([str(z) for z in array[idx0:idx1+1]])
+
+zlevelstring = find_nearest(nc_zlevels, settings.depth_range_to_average)
+
+cmipfilesstring=",".join(cmip_files)
+
+template = jinja_env.get_template("regrid_template.jinja2")
+out = template.render(source_path=source_path,
+                      target_path=target_path,
+                      zlevelstring=zlevelstring,
+                      cmip_files=cmip_files,
+                      cmipfilesstring=cmipfilesstring,
+                      runid=runid)
+
+fname = "cdo_scripts/regrid.sh"
+with open(fname, 'w') as f: f.write(out)
+
+print fname, "written."
+
+# template = jinja_env.get_template("concat_template.jinja2")
+# out = template.render(source_path=source_path,
+#                            target_path=settings.target_path,
+#                            zlevelstring=zlevelstring,
+#                            cmip_files=cmip_files)
+
+# with open("cdo_scripts/concatenate.sh", 'w') as f:
+#     f.write(out)
---
+++ settings.py
@@ -0,0 +1,36 @@
+import os
+
+pcmdipath  = "/p/projects/ipcc_pcmdi/ipcc_ar5_pcmdi/pcmdi_data/"
+target_path = "/p/tmp/mengel/pycmip5/p002_testing/"
+
+gridfiles = "/p/projects/pism/mengel/pism_input/cdo_remapgrids/"
+
+""" cmpip5 model, scenario and run choices """
+
+# models = [ 'ACCESS1-0','ACCESS1-3','bcc-csm1-1','bcc-csm1-1-m','BNU-ESM','CanESM2',
+# 'CCSM4','CESM1-BGC','CESM1-CAM5','CESM1-FASTCHEM','CESM1-WACCM','CNRM-CM5',
+# 'CSIRO-Mk3-6-0','EC-EARTH','FGOALS-g2','FGOALS-s2','FIO-ESM','GFDL-CM3','GFDL-ESM2G',
+# 'GFDL-ESM2M','HadGEM2-CC','HadGEM2-ES','inmcm4','IPSL-CM5A-LR','IPSL-CM5A-MR',
+# 'IPSL-CM5B-LR','MIROC4h','MIROC5','MIROC-ESM','MIROC-ESM-CHEM','MPI-ESM-LR',
+# 'MPI-ESM-MR','MPI-ESM-P','MRI-CGCM3','NorESM1-M','NorESM1-ME']
+
+#models = ['ACCESS1-0','ACCESS1-3']
+models = ['HadGEM2-CC']
+
+#scenarios = ['piControl','historical','rcp85']
+scenarios = ['rcp45']#,'rcp45','rcp60']
+run = "r1i1p1"
+
+variable = "thetao"
+time_res = "Omon"
+cmip5_runid = "r1i1p1"
+
+latitude_bounds = [-90,-60]
+
+# in m, only relevant for 3d fields, in ascending order
+depth_range_to_average = [300, 1000]
+
+# regrid to that
+target_projection = ""
+
+project_root = os.path.dirname(os.path.abspath(__file__))
